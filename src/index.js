81f048593fb6146fd65f3211607b8ebe433415d1
Ahmad Saleh <ahmadrocks2011@hotmail.com>
2022-08-26 15:35:05 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        //e.target.children[1].value; Same Result for const form
+        // or using DOM to select the value of input user
+        const input = document.querySelector('input#searchByID')
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(res => res.json())
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4');
+                const summary = document.querySelector('section#movieDetails p');
+                title.innerHTML = data.title;
+                summary.innerHTML = data.summary;
+            })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
